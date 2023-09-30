79097645cfc35ba9b5abc966f2f502cc2f43ebf9
SilvinaRomero <sromero.fernandez@gmail.com>
2023-09-30 20:14:58 +0200

    subTotal

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,4 +1,5 @@
 import Gasto from "./Gasto"
+import { formatearCantidad } from "../helpers"
 
 const ListadoGastos = ({
     gastos,
@@ -9,22 +10,37 @@ const ListadoGastos = ({
 }) => {
     const capitalize = (str) => {
         if (str == 'varios') {
-           return 'Gastos Varios'
+            return 'Gastos Varios'
         }
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
 
+    const calcularSubtotal = () => {
+        if (gastosFiltrados.length > 0) {
+            const subTotal = gastosFiltrados.reduce((accumulator, currentValue) => accumulator + currentValue.cantidad, 0);
+            return formatearCantidad(subTotal)
+        }
+    }
+
     return (
         <div className="listado-gastos contenedor">
-
-
-
             {/* // saber si hay un filtro, si no vamos a iterar sobre todos los gastos */}
-
             {
                 filtro ? (
                     <>
                         <h2>{gastosFiltrados.length ? `Gastos de la categoría  ${capitalize(filtro)} (${gastosFiltrados.length})` : `No hay gastos en la categoría  ${capitalize(filtro)}`}</h2>
+                        {gastosFiltrados.length ? (
+                            <div className="descripcion-gasto">
+                                <p className="subtotal">
+                                    Subtotal: {''}
+                                    <span>{calcularSubtotal()}</span>
+                                </p>
+
+                            </div>
+
+                        ) : ('')}
+
+
                         {gastosFiltrados.map(gasto => (
                             <Gasto
                                 key={gasto.id}
